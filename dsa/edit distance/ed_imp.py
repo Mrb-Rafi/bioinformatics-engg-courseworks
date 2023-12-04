63791d65c76bdff1d71966bd97024f2c20a5363f
Mrb Rafi <mrbrafi1971@gmail.com>
2023-12-04 08:39:58 +0600

    Update ed_imp.py

--- dsa/edit distance/ed_imp.py
+++ dsa/edit distance/ed_imp.py
@@ -2,10 +2,9 @@ import os
 from Bio import SeqIO
 from Bio import Entrez
 
-Entrez.email = "A.N.Other@example.com"  # Always tell NCBI who you are
+Entrez.email = "example@bau.edu.bd" 
 filename = "gene.txt"
 if not os.path.isfile(filename):
-    # Downloading...
     net_handle = Entrez.efetch(
         db="nucleotide", id="1124779319", rettype="fasta", retmode="text"
     )
